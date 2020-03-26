a464ad00fc0e48025a71da2c99afc234ede8e170
TungDT <tung.daothanhtung@gmail.com>
2020-03-26 08:50:24 +0700

    wip

--- tail.go
+++ tail.go
@@ -3,8 +3,14 @@ package filefollow
 import (
 	"context"
 	"os"
-	"sync"
+	"runtime"
 	"time"
+
+	"github.com/daominah/gomicrokit/gofast"
+	"github.com/daominah/gomicrokit/log"
+	"io/ioutil"
+	"bufio"
+	"bytes"
 )
 
 // Follower _
@@ -17,33 +23,85 @@ type Follower struct {
 	// default assume writer append the file
 	isWriterTruncate bool
 
-	fileDescriptor *os.File
-	outputChan     chan []byte
-	// call this func to stop the Follower (clean resource))
-	stopCxl context.CancelFunc
-	// many goroutines updates the fileDescriptor
-	mutex *sync.Mutex
+	// file descriptor
+	fd           *os.File
+	lastFileInfo os.FileInfo
+	// save where to continue to read if the file got appended
+	lastOffset   int
+	outputChan   chan []byte
+	stopDoneChan <-chan struct{}
+	stopCxl      context.CancelFunc
 }
 
 // NewFollower init a Follower,
 // read Follower fields comment for input meaning
-func NewFollower(filePath string, pollInterval time.Duration,
-	isWriterTruncate bool) *Follower {
+func NewFollower(filePath string, isWriterTruncate bool) *Follower {
+	ctx, cxl := context.WithCancel(context.Background())
 	ret := &Follower{
 		filePath:         filePath,
-		pollInterval:     pollInterval,
+		pollInterval:     100 * time.Millisecond,
 		isWriterTruncate: isWriterTruncate,
+
+		outputChan:   make(chan []byte),
+		stopDoneChan: ctx.Done(),
+		stopCxl:      cxl,
 	}
+	go ret.Follow()
 	return ret
 }
 
 // Follow reads until EOF, send data to the outputChan and
 // wait for file modification
-func (flr Follower) Follow() {
+func (flr *Follower) Follow() {
+	for i := 0; i > -1; i++ {
+		select {
+		case <-flr.stopDoneChan:
+			break
+		default:
+			// pass
+		}
+		if flr.fd == nil {
+			fd, err := os.Open(flr.filePath)
+			if err != nil {
+				if i == 0 {
+					log.Debugf("error when first time os_Open: %v", err)
+				}
+				time.Sleep(flr.pollInterval)
+				continue
+			}
+			flr.fd = fd
+		}
+		bufio.NewReader(flr.fd)
+	}
 }
 
-func (flr Follower) checkFileChanged() bool {
-	f, _ := os.Stat("")
-	f.ModTime()
-	return false
+// checkFileChanged modifies flr_lastFileInfo
+func (flr *Follower) checkFileChanged() (
+	isNewFile bool, isModified bool, err error) {
+	fi, err := os.Stat(flr.filePath)
+	if err != nil {
+		if os.IsNotExist(err) || (runtime.GOOS == "windows" && os.IsPermission(err)) {
+			return true, true, nil
+		}
+		return false, false, err
+	}
+
+	// first time successfully stat the file
+	if gofast.CheckNilInterface(flr.lastFileInfo) {
+		flr.lastFileInfo = fi
+		return true, true, nil
+	}
+
+	// file got moved or renamed
+	if !os.SameFile(flr.lastFileInfo, fi) {
+		return true, true, nil
+	}
+
+	// file got truncated or appended
+	if flr.lastFileInfo.ModTime() != fi.ModTime() {
+		return false, true, nil
+	}
+
+	// file has no changes
+	return false, false, nil
 }
